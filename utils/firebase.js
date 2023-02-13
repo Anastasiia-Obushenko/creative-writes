605dff2b0be39853e34151e9c6ff96a49ed4f1b3
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-13 16:53:25 -0500

    typos fix

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase";
+import { getAuth } from "firebase/auth";
 import { getFireStore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,10 +11,10 @@ import { getFireStore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_app_ID,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASURMENT_ID
 };
 
