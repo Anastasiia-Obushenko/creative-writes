31279c253bad5dff99af4a244915c2996463b4d2
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-13 18:18:59 -0500

    delete analytics

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFireStore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth();
-export const db = getFireStore();
+export const db = getFirestore();
