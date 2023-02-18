1ef1c068021dcd9c33d8cc6f03581e71302579ba
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-17 20:16:50 -0500

    posts appearing on home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Message from '@/components/message';
+import { useEffect, useState } from 'react';
+import { db } from '@/utils/firebase';
+import { async } from '@firebase/util';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
 export default function Home() {
+  // Create a state with all the posts
+  const [allPosts, setAllPosts] = useState([]);
+  const getPosts = async () => {
+    const collectionRef = collection(db, 'posts');
+    const q = query(collectionRef, orderBy('timestamp', 'desc'));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    console.log("a");
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <>
       <Head>
@@ -9,8 +29,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      </main>
+      <div className=' my-12 text-lg font-medium'>
+        <h2>
+          See what other people are saying
+        </h2>
+        {allPosts.map(post => (
+          <Message{...post}>
+
+          </Message>
+        ))}
+      </div>
     </>
-  )
+  );
 }
